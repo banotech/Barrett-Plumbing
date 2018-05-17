c594b497490cc16ef0ad0d57e701ff1c3a201434
banotech <colin@kpk.us>
2018-05-16 20:20:38 -0500

    dev-test
    API tweak (again)

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -1,4 +1,4 @@
-var apiLevel = "14";
+var apiLevel = "23";
 var app;
 function setupApp(){
 	var prefTheme = "md";
@@ -8,7 +8,7 @@ function setupApp(){
 		  root: '#app',
 		  name: 'Barrett Plumbing',
 		  id: 'tech.bano.bp', 
-		  version: "0.1.3114",
+		  version: "0.1.3123",
 		  panel: {
 		    swipe: 'left',
 		  },
